0b0b45089154a6c7466b55cc74bff603be50a42c
programa222 <j_medina@outlook.com>
2022-03-07 00:26:18 -0300

    yaksi

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -51,7 +51,7 @@ function resta(x, y) {
 function multiplica(x, y) {
   // Multiplica "x" por "y" y devuelve el valor
   // Tu código:
-  return x * y;
+return x * y
 }
 
 function divide(x, y) {
@@ -65,24 +65,28 @@ function sonIguales(x, y) {
   // Devuelve "true" si "x" e "y" son iguales
   // De lo contrario, devuelve "false"
   // Tu código:
-  
-  
-}
-
+  if (x === y){
+    return true;
+   } else {
+    return false;
+   }
+  }
 function tienenMismaLongitud(str1, str2) {
   // Devuelve "true" si las dos strings tienen la misma longitud
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+    return "a.length" === "b.length";
 }
 
 function menosQueNoventa(num) {
   // Devuelve "true" si el argumento de la función "num" es menor que noventa
   // De lo contrario, devuelve "false"
   // Tu código:
-  if (num<90)
-  {return true;}
-  return false;
+  if(num < 90) {
+    return true;
+  } else {
+    return false;
+  }
   
 }
 
@@ -90,26 +94,37 @@ function mayorQueCincuenta(num) {
   // Devuelve "true" si el argumento de la función "num" es mayor que cincuenta
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+  return num > 50;
 }
 
 function obtenerResto(x, y) {
   // Obten el resto de la división de "x" entre "y"
   // Tu código:
-  
+  return x % y;
 }
 
 function esPar(num) {
   // Devuelve "true" si "num" es par
   // De lo contrario, devuelve "false"
   // Tu código:
+  if ( num % 2 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+  
   
 }
 
 function esImpar(num) {
   // Devuelve "true" si "num" es impar
   // De lo contrario, devuelve "false"
-  // Tu código:
+  // Tu código: 
+  if ( num % 2 === 1) {
+    return true;
+  } else {
+    return false;
+  }
   
 }
 
@@ -117,36 +132,50 @@ function elevarAlCuadrado(num) {
   // Devuelve el valor de "num" elevado al cuadrado
   // ojo: No es raiz cuadrada!
   // Tu código:
+  return num * num
   
 }
 
 function elevarAlCubo(num) {
   // Devuelve el valor de "num" elevado al cubo
   // Tu código:
+  return num * num * num
   
 }
 
 function elevar(num, exponent) {
   // Devuelve el valor de "num" elevado al exponente dado en "exponent"
   // Tu código:
-  
+
+  return Math.pow (num, exponent);
+
 }
 
 function redondearNumero(num) {
   // Redondea "num" al entero más próximo y devuélvelo
-  // Tu código:
+  // Tu código: 
+
+  var resto = num % 1; 
+  var entero = num - resto;
+     if (resto >= 0.5) {
+         return entero + 1;
+  }  else {
+         return entero;
+  } 
   
 }
 
 function redondearHaciaArriba(num) {
   // Redondea "num" hacia arriba (al próximo entero) y devuélvelo
   // Tu código:
+  return Math.ceil (num)
   
 }
 
 function numeroRandom() {
   //Generar un número al azar entre 0 y 1 y devolverlo
   //Pista: investigá qué hace el método Math.random()
+  return Math.random () 
   
 }
 
